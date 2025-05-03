8fe19b1b04f79a15ce7223488eb563a6d2504e3e
“nourzayati” <nour.zayati@esprit.tn>
2025-05-03 21:52:52 +0100

    Initial commit of Django project

---
+++ pages/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import homePageView
+from .views import index,listBooks,show,add,edit,remove,add_with_form
+
+
+
+
+urlpatterns=[
+    path('',homePageView,name='home'),
+    path('index',index,name="index"),
+    path('books',listBooks,name='listBooks'),
+    path('<int:book_id>/', show,name='show'),
+    path('ajouter_livre/', add ,name='add'),
+    path('modifier_livre/', edit,name='edit'),
+    path('supprimer_livre/', remove, name='remove'),
+    path('ajouter_livre_form/', add_with_form, name='add book form')
+
+    ]
+
+
+
